a4ba481c4686b8dc80fb61c1e521f97790d9b5c0
Eduardo Alves <edu.alves2003@gmail.com>
2024-10-20 15:07:53 -0300

    estrutura geral do codigo criada

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+typedef struct{
+  char name[50];
+  char cpf[15];
+  char cont_tel[10];
+  char email[100]
+} Cliente;
+
+typedef struct{
+  char nome[30];
+  float preco;
+} prod;
+
+
+/// funcoes de cliente
+void adiciona_cliente(Cliente);
+void remove_cliente(char cpf[]);
+void ver_clientes();
+
+/// funcoes pedidos
+void fazer_pedido(char cpf_cliente[], prod *lista_prod[], int viagem);
+void ver_pedidos();
+
+void fazer_reserva(Cliente cliente, int qtd_pessoas, int dia, int mes, int hora);
 
 int main(void){
 
-  printf ("hello, world!!!");
+
 
   return 0;
 }
