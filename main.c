cbf100a9665f24c43d5e41d30ad4b762889cd3b8
Eduardo Alves <edu.alves2003@gmail.com>
2024-10-22 11:01:38 -0300

    menu principal iniciado, gitignore atualizado

--- main.c
+++ main.c
@@ -1,34 +1,91 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <conio.h>
 #include<time.h>
+#include<stdbool.h>
+#include <windows.h>
+
+#define esc 27
 
 typedef struct{
-  char name[50];
-  char cpf[15];
-  char cont_tel[10];
-  char email[50]
+  char *name[50];
+  char *cpf[15];
+  char *cont_tel[10];
+  char *email[50]
 } Cliente;
 
 typedef struct{
-  char nome[30];
+  char *nome[30];
   float preco;
-} prod;
+} Prod;
 
 
 /// funcoes de cliente
-void adiciona_cliente(Cliente);
+void adiciona_cliente(Cliente cliente);
 void remove_cliente(char cpf[]);
 void ver_clientes();
 
 /// funcoes pedidos
-void fazer_pedido(char cpf_cliente[], prod *lista_prod[], int viagem);
+void fazer_pedido(char cpf_cliente[], Prod *lista_prod[], int viagem);
 void ver_pedidos();
 
 void fazer_reserva(Cliente cliente, int qtd_pessoas, int dia, int mes, int hora);
 
+void menu_principal();
+
+void menu_principal(){
+  printf("\n\t  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+  printf("\n\t |                                                         |");
+  printf("\n\t | [1] - cardapio                                          |");
+  printf("\n\t | [2] - Gerenciar clientes                                |");
+  printf("\n\t | [3] - Gerenciar pedidos                                 |");
+  printf("\n\t | [4] - Fazer reserva                                     |"); 
+  printf("\n\t | [ESC]- Voltar                                           |");
+  printf("\n\t  ");
+}
+
+
 int main(void){
+  char menu_opt;
+  bool rodando_menu = true; 
 
-printf('teste');
+do{
+  menu_principal();
 
+  menu_opt = getch();
+
+switch (menu_opt){
+  case '1':
+    system("cls");
+    printf("mostra o cardapio");
+    system("cls");
+    break;
+  case '2':
+    system("cls");
+    printf("abre o menu de gerencimento de clientes");
+    system("cls");
+  break;
+  case '3':
+    system("cls");
+    printf("abre o menu de gerenciamente de pedidos");
+    system("cls");
+  break;
+  case '4':
+    system("cls");
+    printf("abre o menu de gerenciamento de reservas");
+    system("cls");
+  break;
+  case esc:
+    system("cls");
+    rodando_menu = false;
+    system("cls");
+  break;
+  default:
+    system("cls");
+    printf("digite uma opcao valida");
+    system("cls");
+  break;
+}
+  } while(rodando_menu == true);
   return 0;
 }
