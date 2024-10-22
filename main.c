db4feba59d3ecfa3a121ecb3f6801e7d16a931ef
Tainara Lachowski <tailachowski@gmail.com>
2024-10-22 17:29:10 -0300

    cardápio e preços

--- main.c
+++ main.c
@@ -32,6 +32,9 @@ void ver_pedidos();
 void fazer_reserva(Cliente cliente, int qtd_pessoas, int dia, int mes, int hora);
 
 void menu_principal();
+void ver_cardapio();
+void tabela_preco();
+
 
 void menu_principal(){
   printf("\n\t  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
@@ -42,6 +45,56 @@ void menu_principal(){
   printf("\n\t | [4] - Fazer reserva                                     |"); 
   printf("\n\t | [ESC]- Voltar                                           |");
   printf("\n\t  ");
+}
+
+
+
+void ver_cardapio(){
+  printf(" \n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
+  printf("\n|          *Carnes*            |");
+  printf("\n| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
+  printf("\n|       Picanha Grelhada       |");
+  printf("\n|   Mignon na Mostarda e Mel   |");
+  printf("\n|        Filet Argentino       |");
+  printf("\n|     Fraldinha na Mostarda    |");
+  printf("\n|      Maminha com Queijo      |");
+  printf("\n|           Short Rib          |");
+  printf("\n|            Coração           |");
+  printf("\n|        Costela de Porco      |");
+  printf(" \n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
+  printf("\n");
+  printf("\n  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
+  printf("\n|        *Massas/Risoto*       |");
+  printf("\n| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
+  printf("\n|         Nhoque Frito         |");
+  printf("\n|    Lasanha Quatro Queijos    |");
+  printf("\n|     Conchiglione de Figo     |");
+  printf("\n|         Fettuccini           |");
+  printf("\n|    Banana com Chocolate      |");
+  printf("\n|      Nhoque Dois Amores      |");
+  printf("\n|      Risoto de Palmito       |");
+  printf("\n|      Risoto de Camarão       |");
+  printf(" \n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
+  printf("\n");
+  printf(" \n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
+  printf("\n|        *Sobremesas*          |");
+  printf("\n| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
+  printf("\n|            Tortas            |");
+  printf("\n|            Mousses           |");
+  printf("\n|             Pavês            |");
+  printf(" \n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  \n");
+  printf("obs: confira a opção disponível no dia");
+}
+
+
+
+void tabela_preco(){
+  printf("\n  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
+  printf("\n|             *Tabela de Preços*              |");
+  printf("\n| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |");
+  printf("\n| De terça a sexta.......R$ 149,90/por pessoa |");
+  printf("\n| Sábado e Domingo.......R$ 159,90/por pessoa |");
+  printf("\n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
 }
 
 
@@ -57,7 +110,8 @@ do{
 switch (menu_opt){
   case '1':
     system("cls");
-    printf("mostra o cardapio");
+    ver_cardapio();
+    tabela_preco();
     system("cls");
     break;
   case '2':
